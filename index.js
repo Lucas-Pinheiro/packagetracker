3b6ba051452dfbc47e48c0ea8b50574368bfbbfd
Lucas Pinheiro <lucas.contato1996@gmail.com>
2016-04-21 23:28:07 -0300

    body parser

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require("express");
+var body_parser = require("body-parser");
 
 
 var app = express();
 
 app.set("port", (process.env.PORT));
+app.use(bodyParser.json());
 
 app.get("/", (request, response) => {
     response.send("Just a Facebook messeger bot webhook, nothing here yet :D.");
