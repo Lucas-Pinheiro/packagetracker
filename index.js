f20dfe45590e4f7c2722d257f05d03081709e43d
Lucas Pinheiro <lucas.contato1996@gmail.com>
2016-04-21 23:03:06 -0300

    validation

--- index.js
+++ index.js
@@ -5,10 +5,17 @@ var app = express();
 
 app.set("port", (process.env.PORT));
 
-app.get("/", function(request, response) {
-    response.send("Hello");
+app.get("/", (request, response) => {
+    response.send(process.env.SECRET);
 });
 
-app.listen(app.get("port"), function() {
+app.get("/webhook", (request, response) => {
+    if (request.query["hub.verify_token"] === process.env.SECRET)
+        response.send(request.query["hub.challenge"]);
+
+    response.send("Error, wrong validation token");
+});
+
+app.listen(app.get("port"), () => {
     console.log("Smough is now running at port: ", app.get("port"));
 });
